3ccb4838fdbd958f3a56abec2185fc56da625b4f
sarah <xy_sarah_qd@163.com>
2018-11-19 12:09:25 +0800

    初始化仓库

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    这是一个app组件
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/layouts.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="container">
+        <section class="menu">左边的容器</section>
+        <section class="content-container">右边的容器</section>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="less">
+  
+</style>
+
+
