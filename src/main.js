8c6717b75f272c5fa31114cc73fe8dba062035e4
sarah <xy_sarah_qd@163.com>
2018-11-19 20:41:06 +0800

    六宫格导航完成

--- src/main.js
+++ src/main.js
@@ -1,15 +1,25 @@
 import Vue from 'vue'
+//导入app根组件
 import App from './App'
+//导入路由模块
 import router from './router'
+import VueResource from 'vue-resource'
+
+Vue.use(VueResource)
 
 //引入header组件
-import {Header} from 'mint-ui'
+import {Header,Swipe, SwipeItem } from 'mint-ui'
 Vue.component(Header.name, Header)
+Vue.component(Swipe.name, Swipe)
+Vue.component(SwipeItem.name, SwipeItem)
 
 //引入mui
 import './lib/mui/css/mui.css'
 // 导入扩展图标样式
 import './lib/mui/css/icons-extra.css'
+
+//引入自己的样式
+import './common/css/base.css'
 
 Vue.config.productionTip = false
 
--- src/page/home/index.vue
+++ src/page/home/index.vue
@@ -1,15 +1,129 @@
 <template>
-     <h1>home</h1>
+  <div class="home-container">
+    <!-- 轮播图 -->
+    <mt-swipe :auto="4000">
+      <mt-swipe-item v-for="item in bannerList" :key="item.img">
+        <a :href="item.url"><img :src="item.img" alt="" /> </a>
+      </mt-swipe-item>
+      <!--
+        <mt-swipe-item>2</mt-swipe-item>
+        <mt-swipe-item>3</mt-swipe-item>
+      -->
+    </mt-swipe>
+
+    <!-- 六宫格 -->
+    <ul class="mui-table-view mui-grid-view mui-grid-9">
+      <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3">
+        <a href="#">
+          <img src="../../assets/menu1.png" alt="">
+          <div class="mui-media-body">新闻资讯</div>
+        </a>
+      </li>
+      <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3">
+        <a href="#">
+          <img src="../../assets/menu2.png" alt="">
+          <div class="mui-media-body">图片分享</div>
+        </a>
+      </li>
+      <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3">
+        <a href="#">
+          <img src="../../assets/menu3.png" alt="">
+          <div class="mui-media-body">商品购买</div>
+        </a>
+      </li>
+      <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3">
+        <a href="#">
+          <img src="../../assets/menu4.png" alt="">
+          <div class="mui-media-body">留言反馈</div>
+        </a>
+      </li>
+      <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3">
+        <a href="#">
+          <img src="../../assets/menu5.png" alt="">
+          <div class="mui-media-body">视频专区</div>
+        </a>
+      </li>
+      <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3">
+        <a href="#">
+          <img src="../../assets/menu6.png" alt="">
+          <div class="mui-media-body">联系我们</div>
+        </a>
+      </li>
+    
+    </ul>
+  </div>
 </template>
 
 <script>
-export default {
-    
-}
+  import { Toast } from "mint-ui";
+
+  export default {
+    data() {
+      return {
+        bannerList: []
+      };
+    },
+    created() {
+      this.getBanner();
+    },
+    methods: {
+      getBanner() {
+        //获取轮播图数据的方法
+        this.$http
+          .get("http://vue.lovegf.cn:8899/api/getlunbo")
+          .then(result => {
+            //     console.log(result.body)
+            if (result.body.status == 0) {
+              //成功
+              this.bannerList = result.body.message;
+          //     console.log(this.bannerList)
+            } else {
+              Toast("加载图片失败....");
+            }
+          });
+      }
+    }
+  };
 </script>
 
 <style lang="less">
+  .home-container {
+    .mint-swipe {
+      height: 200px;
+      .mint-swipe-item{
+           &:nth-child(1){
+               background-color: yellow;
+          }
+          &:nth-child(2){
+               background-color: pink;
+          }
+          a {
+               display: block;
+               width: 100%;
+               height: 100%;
+               img {
+                    display: block;
+                    width: 100%;
+                    height: 100%;
+               }
+          }
+      }
+      
+    }
 
+    .mui-grid-view.mui-grid-9 {
+          background-color: #fff;
+          border: none;
+          .mui-table-view-cell{
+               border:0;
+               img {
+                    width: 60px;
+                    height: 60px;
+               }
+                .mui-media-body{
+                    font-size: 13px;
+               }
+          }        
+     }
+  }
 </style>
-
-
